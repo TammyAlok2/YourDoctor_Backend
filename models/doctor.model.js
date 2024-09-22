8a4ad70fe9d202395d2403dd3c9821638a2f19fa
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-09-22 10:53:48 +0530

    Doctor name should be capital

--- models/doctor.model.js
+++ models/doctor.model.js
@@ -11,7 +11,6 @@ const doctorSchema = new Schema(
       type: String,
       required: [true, 'Name is required'],
       minlength: [5, 'Name must be at least 5 characters'],
-      lowercase: true,
       trim: true, // Removes unnecessary spaces
     },
     email: {
